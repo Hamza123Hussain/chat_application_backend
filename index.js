d12cfba574e9739a78af0fc28dbda2ac01cd20e9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-13 02:53:46 +0500

    allowing search api path

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ import Router from './DB/RegisterandLogin.js'
 import UserRouter from './DB/GettingUserData.js'
 import http from 'http'
 import { startWebSocketServer } from './DB/GettingChatList.js'
+import SearchRouter from './DB/Searchingforuser.js'
 const App = express()
-// Apply CORS middleware with default options (allow all origins)
+
 App.use(cors())
 App.use(express.json())
 
 App.use('/api/User', Router)
 App.use('/api/GetUser', UserRouter)
-
+App.use('/api/Search', SearchRouter)
 const server = http.createServer(App)
 
-// Start the WebSocket server
 startWebSocketServer(server)
 
-// Your other Express routes and middleware...
-
-// Start the HTTP server on port 5000
 server.listen(Port, () => {
   console.log('HTTP server running on http://localhost:5000')
 })
