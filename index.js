10ec0907a1f49844b86714b7a37e174871f0268a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-13 01:57:23 +0500

    backend running through web socket instead of express

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 import { Port } from './Config.js'
 import Router from './DB/RegisterandLogin.js'
 import UserRouter from './DB/GettingUserData.js'
-
+import http from 'http'
+import { startWebSocketServer } from './DB/GettingChatList.js'
 const App = express()
 // Apply CORS middleware with default options (allow all origins)
 App.use(cors())
@@ -12,6 +13,14 @@ App.use(express.json())
 App.use('/api/User', Router)
 App.use('/api/GetUser', UserRouter)
 
-App.listen(Port, () => {
-  console.log(`Running On Port ${Port}`)
+const server = http.createServer(App)
+
+// Start the WebSocket server
+startWebSocketServer(server)
+
+// Your other Express routes and middleware...
+
+// Start the HTTP server on port 5000
+server.listen(Port, () => {
+  console.log('HTTP server running on http://localhost:5000')
 })
