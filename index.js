e868f0b68e01868f6225fa069778ef84f4227781
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 15:46:57 +0500

    allowing cors for all now. will modify once all done

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import { Port } from './Config.js'
+import Router from './DB/PostingUserDetails.js'
 
 const App = express()
-
+// Apply CORS middleware with default options (allow all origins)
+App.use(cors())
 App.use(express.json())
+
+App.use('/api/Register', Router)
 
 App.listen(Port, () => {
   console.log(`Running On Port ${Port}`)
