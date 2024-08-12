e9238d1eff1e7f9612ce43045745714d8d48b1a7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 15:46:04 +0500

    Adding MessagingSenderID

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ const Api = process.env.apiKey
 const authDomain = process.env.authDomain
 const projectId = process.env.projectId
 const appIdmessaging = process.env.appId
-const SenderId = process.env.SenderId
+const messagingSenderId = process.env.messagingSenderId
 const storageBucket = process.env.storageBucket
 export {
   Port,
@@ -12,6 +12,6 @@ export {
   authDomain,
   projectId,
   appIdmessaging,
-  SenderId,
+  messagingSenderId,
   storageBucket,
 }
