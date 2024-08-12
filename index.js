9982f2832c0cd352394ad38ea84a448cbf504683
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 16:25:09 +0500

    allowing api path

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import { Port } from './Config.js'
-import Router from './DB/PostingUserDetails.js'
+import Router from './DB/RegisterandLogin.js'
+import UserRouter from './DB/GettingUserData.js'
 
 const App = express()
 // Apply CORS middleware with default options (allow all origins)
@@ -9,6 +10,7 @@ App.use(cors())
 App.use(express.json())
 
 App.use('/api/User', Router)
+App.use('/api/GetUser', UserRouter)
 
 App.listen(Port, () => {
   console.log(`Running On Port ${Port}`)
