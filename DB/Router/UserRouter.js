4c3d3e58de0eb03b9d0ca160da623c244237a732
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-28 17:47:25 +0500

    added new routes to create a message

--- DB/Router/UserRouter.js
+++ DB/Router/UserRouter.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { GetUser } from '../Controllers/User/GettingUserDetails.js'
 import { SearchUser } from '../Controllers/User/SearchUser.js'
+import { CreateMssage } from '../Controllers/User/CreatingandGettingMessages.js'
+
 const UserRouter = express.Router()
 UserRouter.get('/GetUser', GetUser)
 /**sending userID as a query */
 UserRouter.get('/SearchUser', SearchUser)
 /**sending name as a query */
+UserRouter.post('/NewMessage', CreateMssage)
 export default UserRouter
