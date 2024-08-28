8fb7e965a5971975cc09b66041e19811c510e0e3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-28 20:17:53 +0500

    api getting the chatlist

--- DB/Router/ChatRouter.js
+++ DB/Router/ChatRouter.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { GetChat } from '../Controllers/Chat/GetChat.js'
 import { CreateChat } from '../Controllers/Chat/CreateChat.js'
+import { GetChats } from '../Controllers/Chat/GettingChatList.js'
 const ChatRouter = express.Router()
 ChatRouter.get('/GetChat', GetChat)
 /**sending ChatID as a query */
 ChatRouter.post('/CreateChat', CreateChat)
+ChatRouter.get('/GetChatList', GetChats)
 export default ChatRouter
