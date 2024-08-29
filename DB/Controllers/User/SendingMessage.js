9fe3201d824aaad7d4900af658d0a1bb80770ad5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-29 07:58:50 +0500

    chnaged the name of the file

--- DB/Controllers/User/SendingMessage.js
+++ DB/Controllers/User/SendingMessage.js
@@ -1,18 +1,14 @@
 import { arrayUnion, doc, updateDoc, getDoc } from 'firebase/firestore'
 import { db } from '../../../FirebaseConfig.js'
-
-export const CreateMssage = async (req, res, io) => {
+export const CreateMssage = async (req, res) => {
   try {
     const { chatID, text, senderId, receiverId } = req.body
-
     // Validate required fields
     if (!chatID || !text || !senderId || !receiverId) {
       return res.status(400).json({ error: 'Missing required fields' })
     }
-
     // Reference to the chat document
     const chatDocumentRef = doc(db, 'userchats', chatID)
-
     // Update the document by pushing the new message to the messages array
     await updateDoc(chatDocumentRef, {
       messages: arrayUnion({
@@ -21,24 +17,19 @@ export const CreateMssage = async (req, res, io) => {
         timestamp: new Date(), // Add a timestamp for the message
       }),
     })
-
     // References to the sender and receiver documents in the 'Chats' collection
     const senderDocumentRef = doc(db, 'Chats', senderId)
     const receiverDocumentRef = doc(db, 'Chats', receiverId)
-
     // Get the current chat data for both sender and receiver
     const [senderDocSnap, receiverDocSnap] = await Promise.all([
       getDoc(senderDocumentRef),
       getDoc(receiverDocumentRef),
     ])
-
     if (!senderDocSnap.exists() || !receiverDocSnap.exists()) {
       return res.status(404).json({ error: 'Chat data not found' })
     }
-
     const senderChats = senderDocSnap.data().chats || []
     const receiverChats = receiverDocSnap.data().chats || []
-
     // Function to update the chat array with the latest message
     const updateChatArray = (chats, chatID, lastMessage) => {
       return chats.map((chat) =>
@@ -51,7 +42,6 @@ export const CreateMssage = async (req, res, io) => {
           : chat
       )
     }
-
     // Update the chats for sender and receiver
     await Promise.all([
       updateDoc(senderDocumentRef, {
@@ -61,20 +51,8 @@ export const CreateMssage = async (req, res, io) => {
         chats: updateChatArray(receiverChats, chatID, text),
       }),
     ])
-
-    // Emit a message event to all connected clients
-    io.emit('message', {
-      chatID,
-      senderId,
-      receiverId,
-      text,
-      timestamp: new Date(),
-    })
-
     // Send a successful response
-    res
-      .status(200)
-      .json({ message: 'Message created and chats updated successfully' })
+    res.status(200).json({ senderId, text, timestamp: new Date() })
     console.log('Message created and chats updated successfully')
   } catch (error) {
     console.error('Error creating message:', error)
