7d518cca05563c59d793c9e7f7f12c096aca2afa
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 03:24:41 +0500

    adding a message ID As It could help us delete messages

--- DB/Controllers/User/SendingMessage.js
+++ DB/Controllers/User/SendingMessage.js
@@ -1,8 +1,10 @@
 import { arrayUnion, doc, updateDoc, getDoc } from 'firebase/firestore'
 import { db, storage } from '../../../FirebaseConfig.js'
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { v4 as uuid } from 'uuid'
 export const CreateMssage = async (req, res) => {
   try {
+    const Random = uuid()
     const { chatID, text, senderId, receiverId } = req.body
     // Validate required fields
     if (!chatID || !text || !senderId || !receiverId) {
@@ -31,6 +33,7 @@ export const CreateMssage = async (req, res) => {
     // Update the document by pushing the new message to the messages array
     await updateDoc(chatDocumentRef, {
       messages: arrayUnion({
+        MessageID: uuid(),
         senderId,
         text,
         timestamp: new Date(), // Add a timestamp for the message
