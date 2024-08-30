2db986ea7d9bd07c3051b5e0e8d5f5b9e3529748
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-30 20:56:51 +0500

    added the file middleware

--- DB/Router/UserRouter.js
+++ DB/Router/UserRouter.js
@@ -2,11 +2,12 @@ import express from 'express'
 import { GetUser } from '../Controllers/User/GettingUserDetails.js'
 import { SearchUser } from '../Controllers/User/SearchUser.js'
 import { CreateMssage } from '../Controllers/User/SendingMessage.js'
-
+import multer from 'multer'
+const upload = multer({ storage: multer.memoryStorage() })
 const UserRouter = express.Router()
 UserRouter.get('/GetUser', GetUser)
 /**sending userID as a query */
 UserRouter.get('/SearchUser', SearchUser)
 /**sending name as a query */
-UserRouter.post('/NewMessage', CreateMssage)
+UserRouter.post('/NewMessage', upload.single('File'), CreateMssage)
 export default UserRouter
