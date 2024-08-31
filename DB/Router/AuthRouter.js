36434caab55a457867fcad85bb666664d8b6905b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 19:39:44 +0500

    added reset password controller and route

--- DB/Router/AuthRouter.js
+++ DB/Router/AuthRouter.js
@@ -3,9 +3,11 @@ import express from 'express'
 import { Register } from '../Controllers/Auth/Register.js'
 import { Login } from '../Controllers/Auth/Login.js'
 import { Signout } from '../Controllers/Auth/Signout.js'
+import ResetPass from '../Controllers/Auth/ResetPass.js'
 const upload = multer({ storage: multer.memoryStorage() })
 const AuthRouter = express.Router()
 AuthRouter.post('/Register', upload.single('File'), Register)
 AuthRouter.post('/Login', Login)
 AuthRouter.get('/SignOut', Signout)
+AuthRouter.post('/Reset', ResetPass)
 export default AuthRouter
